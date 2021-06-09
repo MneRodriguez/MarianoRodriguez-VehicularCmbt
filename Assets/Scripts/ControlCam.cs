170ac647e3959283f3207f4bb5e3852fb4e77559
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-06-09 15:42:33 -0300

    Control de camara pulido

---
+++ Assets/Scripts/ControlCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCam : MonoBehaviour
+{
+    public GameObject CañoñDelPlayer;
+    public Vector3 offset;
+    public float movtoCam = 0.5f;
+    void Start()
+    {
+        offset = transform.position + CañoñDelPlayer.transform.position;
+    }
+
+    
+    void Update()
+    {
+        Vector3 posDeCam = new Vector3(CañoñDelPlayer.transform.position.x, CañoñDelPlayer.transform.position.y +0.250f, CañoñDelPlayer.transform.position.z -0.78f);
+
+        transform.position = Vector3.SmoothDamp(transform.position, posDeCam, ref offset, movtoCam);
+    }
+}
