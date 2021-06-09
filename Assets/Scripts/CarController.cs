c5a0a181c4444faad15f05e24eb216e69d04eec4
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-06-09 17:58:18 -0300

    Perfeccionando control sobre el auto

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public WheelCollider RuedaFrntIzq;
+    public WheelCollider RuedaFrntlDer;
+    public WheelCollider RuedaTraseraIzq;
+    public WheelCollider RuedaTraseraDer;
+
+    public Transform RuedaFrntIzqTransfm;
+    public Transform RuedaFrntDerTransfm;
+    public Transform RuedaTraseraIzqTransfm;
+    public Transform RuedaTraseraDerTransfm;
+
+    public float anguloGiro = 45;
+    public float maxTorque = 1000;
+    public float maxTorqueFreno = 500;
+
+    public Transform centroDeMasa;
+
+    float gravedad = 9.8f;
+    bool frenado = false;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centroDeMasa.transform.localPosition;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!frenado)
+        {
+            RuedaFrntIzq.brakeTorque = 0;
+            RuedaFrntlDer.brakeTorque = 0;
+            RuedaTraseraIzq.brakeTorque = 0;
+            RuedaTraseraDer.brakeTorque = 0;
+
+            // VELOCIDAD O MOVIMIENTO DEL AUTO MEDIANTE INPUT DEL TECLADO
+            RuedaTraseraIzq.motorTorque = maxTorque * Input.GetAxis("Vertical");
+            RuedaTraseraDer.motorTorque = maxTorque * Input.GetAxis("Vertical");
+
+            // CAMBIO EN LA DIRECCION DEL AUTO
+            RuedaFrntIzq.steerAngle = anguloGiro * Input.GetAxis("Horizontal");
+            RuedaFrntlDer.steerAngle = anguloGiro * Input.GetAxis("Horizontal");
+        }
+    }
+
+    void Update()
+    {
+        FrenoDeMano();
+
+        // GIRO DE LAS RUEDAS
+        RuedaFrntIzqTransfm.Rotate(RuedaFrntIzq.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        RuedaFrntDerTransfm.Rotate(RuedaFrntlDer.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        RuedaTraseraIzqTransfm.Rotate(RuedaTraseraIzq.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        RuedaTraseraDerTransfm.Rotate(RuedaTraseraDer.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+
+        // CAMBIO EN LA DIRECCION DE LAS RUEDAS
+        Vector3 temp = RuedaFrntIzqTransfm.localEulerAngles;
+        Vector3 temp1 = RuedaFrntDerTransfm.localEulerAngles;
+
+        temp.y = RuedaFrntIzq.steerAngle - RuedaFrntIzqTransfm.localEulerAngles.z;
+        RuedaFrntIzqTransfm.localEulerAngles = temp;
+
+        temp1.y = RuedaFrntlDer.steerAngle - RuedaTraseraDerTransfm.localEulerAngles.z;
+        RuedaTraseraDerTransfm.localEulerAngles = temp1;
+    }
+
+    void FrenoDeMano()
+    {
+        if (Input.GetButton("Jump")) // SI SE PRESIONA LA BARRA ESPACIADORA, FRENA
+        {
+            frenado = true;
+        }
+        else { frenado = false; }
+
+        if (frenado)
+        {
+            RuedaTraseraIzq.brakeTorque = maxTorqueFreno * 20;
+            RuedaTraseraDer.brakeTorque = maxTorqueFreno * 20;
+
+            RuedaTraseraIzq.motorTorque = 0;
+            RuedaTraseraDer.motorTorque = 0;
+        }
+    }
+}
