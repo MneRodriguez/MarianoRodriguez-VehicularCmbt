5d75642799537cfd489ef99dabf2c3465570eafb
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-06-10 18:35:26 -0300

    Añadidas colision contra enemigo y pantallasVictoria-Derrota

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CarController : MonoBehaviour
 {
+        
     public WheelCollider RuedaFrntIzq;
     public WheelCollider RuedaFrntDer;
     public WheelCollider RuedaTraseraIzq;
@@ -14,9 +16,9 @@ public class CarController : MonoBehaviour
     public Transform RuedaTraseraIzqTransfm;
     public Transform RuedaTraseraDerTransfm;
 
-    public float anguloGiro = 45;
-    public float maxTorque = 1000;
-    public float maxTorqueFreno = 500;
+    public float anguloGiro = 45f;
+    public float maxTorque = 600f;
+    public float maxTorqueFreno = 200f;
 
     public Transform centroDeMasa;
 
@@ -30,8 +32,20 @@ public class CarController : MonoBehaviour
         rb.centerOfMass = centroDeMasa.transform.localPosition;
     }
 
+    /*private void actualizarPosRuedas (WheelCollider ruedaCollider, Transform ruedaTransform)
+    {
+        Vector3 pos = ruedaTransform.position;
+        Quaternion quat = ruedaTransform.rotation;
+
+        ruedaCollider.GetWorldPose(out pos, out quat);
+
+        ruedaTransform.position = pos;
+        ruedaTransform.rotation = quat;
+    }*/
     private void FixedUpdate()
     {
+        
+        
         if (!frenado)
         {
             RuedaFrntIzq.brakeTorque = 0;
@@ -39,34 +53,60 @@ public class CarController : MonoBehaviour
             RuedaTraseraIzq.brakeTorque = 0;
             RuedaTraseraDer.brakeTorque = 0;
 
-            // VELOCIDAD O MOVIMIENTO DEL AUTO MEDIANTE INPUT DEL TECLADO
-            RuedaTraseraIzq.motorTorque = maxTorque * Input.GetAxis("Vertical");
-            RuedaTraseraDer.motorTorque = maxTorque * Input.GetAxis("Vertical");
+            // ACELERACION DEL AUTO CON LAS RUEDAS TRASERAS E INPUT "W"
+            RuedaTraseraIzq.motorTorque = maxTorque * Input.GetAxisRaw("Vertical");
+            RuedaTraseraDer.motorTorque = maxTorque * Input.GetAxisRaw("Vertical");
 
-            // CAMBIO EN LA DIRECCION DEL AUTO
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                rb.AddRelativeForce(new Vector3(Vector3.forward.x, 0, Vector3.forward.z) * gravedad);
+                rb.AddForce(Vector3.forward * rb.mass);
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                rb.AddRelativeForce(new Vector3(Vector3.forward.x, 0, Vector3.forward.z) * -gravedad);
+            }
+
+            Vector3 VelLocal = transform.InverseTransformDirection(rb.velocity);
+            VelLocal.x = 0;
+            rb.velocity = transform.TransformDirection(VelLocal);
+
+            
+
+            // CAMBIO EN LA DIRECCION DE LAS RUEDAS DELANTERAS
             RuedaFrntIzq.steerAngle = anguloGiro * Input.GetAxis("Horizontal");
             RuedaFrntDer.steerAngle = anguloGiro * Input.GetAxis("Horizontal");
+
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                rb.AddRelativeTorque(-Vector3.up * anguloGiro);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                rb.AddRelativeTorque(Vector3.up * anguloGiro);
+            }
         }
     }
 
     void Update()
     {
+        
         FrenoDeMano();
 
-        // GIRO DE LAS RUEDAS -- ARREGLAR, HACE QUE GIREN HORIZONTALMENTE Y NO VERTICALMENTE, POR ESO NO PUEDO AVANZAR
-        /*RuedaFrntIzqTransfm.Rotate(RuedaFrntIzq.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        // GIRO DE LAS RUEDAS
+        RuedaFrntIzqTransfm.Rotate(RuedaFrntIzq.rpm / 60 * 360 * Time.deltaTime, 0, 0);
         RuedaFrntDerTransfm.Rotate(RuedaFrntDer.rpm / 60 * 360 * Time.deltaTime, 0, 0);
-        RuedaTraseraIzqTransfm.Rotate(RuedaTraseraIzq.rpm / 60 * 360 * Time.deltaTime, 0, 0);
-        RuedaTraseraDerTransfm.Rotate(RuedaTraseraDer.rpm / 60 * 360 * Time.deltaTime, 0, 0);*/
+        RuedaTraseraIzqTransfm.Rotate(0, RuedaTraseraIzq.rpm / 60 * 360 * Time.deltaTime, 0);
+        RuedaTraseraDerTransfm.Rotate(0, RuedaTraseraDer.rpm / 60 * 360 * Time.deltaTime, 0);
 
         // CAMBIO EN LA DIRECCION DE LAS RUEDAS
         Vector3 temp = RuedaFrntIzqTransfm.localEulerAngles;
         Vector3 temp1 = RuedaFrntDerTransfm.localEulerAngles;
 
-        temp.y = RuedaFrntIzq.steerAngle - RuedaFrntIzqTransfm.localEulerAngles.z;
+        temp.y = RuedaFrntIzq.steerAngle - RuedaFrntIzqTransfm.localEulerAngles.x;
         RuedaFrntIzqTransfm.localEulerAngles = temp;
 
-        temp1.y = RuedaFrntDer.steerAngle - RuedaFrntDerTransfm.localEulerAngles.z;
+        temp1.y = RuedaFrntDer.steerAngle - RuedaFrntDerTransfm.localEulerAngles.x;
         RuedaFrntDerTransfm.localEulerAngles = temp1;
     }
 
@@ -87,4 +127,13 @@ public class CarController : MonoBehaviour
             RuedaTraseraDer.motorTorque = 0;
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemigo"))
+        {
+            SceneManager.LoadScene("SceneDerrota");
+        }
+    }
 }
+
