438d5e4bd3458636ed2ab613cd10b295458e74b1
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-06-11 16:45:21 -0300

    Condicion Victoria-Derrota -listo

--- Assets/Scripts/IAenems.cs
+++ Assets/Scripts/IAenems.cs
@@ -1,28 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IAenems : MonoBehaviour
 {
-    public Transform puntoDeDestino;
+    public GameObject Enem;
+    public Rigidbody rbEnem;
+
+    /*public Transform puntoDeDestino;
     Rigidbody rb;
-    public float speed = 10.5f;
+    public float speed = 10.5f;*/
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rbEnem = GetComponent<Rigidbody>();
+
     }
 
     
     void Update()
     {
-        rb.AddForce(CalcularVector(), ForceMode.Force);
+        //rb.AddForce(CalcularVector(), ForceMode.Force);
     }
 
-    private Vector3 CalcularVector()
+    /*private Vector3 CalcularVector()
     {
         Vector3 VectorDeDistancia = (puntoDeDestino.position - transform.position);
         float distancia = VectorDeDistancia.magnitude;
         speed = Mathf.Lerp(distancia, 0, Time.deltaTime);
         return Vector3.Normalize(VectorDeDistancia) * speed;
+    }*/
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("BalaJugador"))
+        {
+            ContadorScore.valorScore += 1;
+            Destroy(gameObject);
+        }
+
+        if (ContadorScore.valorScore == 3)
+        {
+            SceneManager.LoadScene("SceneVictoria");
+        }
     }
 }
