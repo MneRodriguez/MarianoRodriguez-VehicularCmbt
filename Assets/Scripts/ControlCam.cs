60f741bfc153201e6970f65cd7daaed35056dc91
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-06-12 17:15:40 -0300

    Rotacion de camara mientras sigue al player -hecho

--- Assets/Scripts/ControlCam.cs
+++ Assets/Scripts/ControlCam.cs
@@ -7,6 +7,9 @@ public class ControlCam : MonoBehaviour
     public GameObject CañoñDelPlayer;
     public Vector3 offset;
     public float movtoCam = 0.5f;
+
+    public Transform cañonJgdrParaGiroCam;
+    public float velGiroDeCam = 0.8f;
     void Start()
     {
         offset = transform.position + CañoñDelPlayer.transform.position;
@@ -18,5 +21,12 @@ public class ControlCam : MonoBehaviour
         Vector3 posDeCam = new Vector3(CañoñDelPlayer.transform.position.x, CañoñDelPlayer.transform.position.y +0.250f, CañoñDelPlayer.transform.position.z -0.78f);
 
         transform.position = Vector3.SmoothDamp(transform.position, posDeCam, ref offset, movtoCam);
+    }
+
+    private void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Horizontal") * velGiroDeCam, Vector3.up) * offset;
+        transform.position = cañonJgdrParaGiroCam.position + offset;
+        transform.LookAt(cañonJgdrParaGiroCam.position);
     }
 }
